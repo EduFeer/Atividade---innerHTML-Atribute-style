f2faa285c1f98d7de125a6663413c29398875397
Eduardo Ferreira <79938371+EduFeer@users.noreply.github.com>
2021-03-11 09:54:25 -0300

    Add files via upload

---
+++ inner.js
@@ -0,0 +1,40 @@
+document.getElementById("entrar").innerHTML = "teste";
+document.getElementById("entrar").style.color = "blue";
+document.getElementById("entrar").style.background = "lightblue";
+
+document.getElementById("sair").innerHTML = "Apartir de agora eu odeio a letra s";
+document.getElementById("sair").style.color = "blue";
+document.getElementById("sair").style.background = "lightblue";
+
+
+document.getElementById("oi").innerHTML = "A partir de agora todos contra o s";
+document.getElementById("oi").style.color = "blue";
+document.getElementById("oi").style.background = "lightblue";
+
+document.getElementById("tim").innerHTML = "jdiof";
+document.getElementById("tim").style.color = "blue";
+document.getElementById("tim").style.background = "lightblue";
+
+document.getElementById("vivo").innerHTML = "SAS";
+document.getElementById("vivo").style.color = "blue";
+document.getElementById("vivo").style.background = "lightblue";
+
+document.getElementById("claro").innerHTML = "masj";
+document.getElementById("claro").style.color = "blue";
+document.getElementById("claro").style.background = "lightblue";
+
+document.getElementById("e-mail").placeholder = "E-mail";
+document.getElementById("e-mail").style.background = "black";
+
+document.getElementById("password").placeholder = "senha";
+document.getElementById("password").style.background = "black";
+
+document.getElementById("fundo").style.background = "blue";
+
+document.getElementById("borda").style.border = "dotted 10px hotpink";
+document.getElementById("borda").style.background = "lightblue";
+
+document.getElementById("link").style.border = "dotted 2px red";
+document.getElementById("link").style.background = "black";
+
+document.getElementById("teste").innerHTML = "teste";
